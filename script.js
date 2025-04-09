6439f76e52f521657aa73f48fa7e7136cb821450
thespawnspy <alex@spydesign.ca>
2025-04-09 09:31:51 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
 document.getElementById("predictionForm").addEventListener("submit", function(event) {
   event.preventDefault();
-  const form = event.target;
-  const formData = new FormData(form);
-  const data = {};
-  formData.forEach((value, key) => {
-    data[key] = value;
-  });
-
-  fetch("https://script.google.com/macros/s/AKfycbzxkr5Aoc2P8_5AxzSnLvbrwU-yCPXB0kyQTzvZX7nK4Eq485BIMgoUmAKgGlHe2QjW/exec", {
-    method: "POST",
-    mode: "no-cors",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(data)
-  }).then(() => {
-    alert("Prediction submitted!");
-    form.reset();
-  }).catch(() => {
-    alert("Error submitting form.");
-  });
+  alert("Prediction submitted!");
 });
